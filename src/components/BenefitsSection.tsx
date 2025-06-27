a58b1a52fc8b45e46f9110f78d0738eab1166090
GabrielC-Web <dobbyeslibre23@gmail.com>
2025-06-26 21:48:00 -0400

    changes in benefits section

--- src/components/BenefitsSection.tsx
+++ src/components/BenefitsSection.tsx
@@ -1,32 +1,41 @@
+import { Scaling, ShieldCheck, Sparkles } from "lucide-react";
+import "../styles/benefits.css";
 import { AnimatedChar } from "./AnimatedChar";
 
 export default function BenefitsSection() {
   //* Title
   const text: any = "What benefits do I offer to you?".split(" ");
 
-  // const benefits = [
-  //   {
-  //     icon: <Scaling className="w-10 h-10 text-orange-700 mb-4" />,
-  //     title: "Scalable & High-Performance",
-  //     description:
-  //       "Building robust applications capable of handling high traffic, like payment gateways with over 20,000 daily transactions.",
-  //   },
-  //   {
-  //     icon: <Sparkles className="w-10 h-10 text-orange-700 mb-4" />,
-  //     title: "Efficient & Modern Code",
-  //     description:
-  //       "Leveraging reusable component libraries and modern architecture to cut down on maintenance time and ensure consistency.",
-  //   },
-  //   {
-  //     icon: <ShieldCheck className="w-10 h-10 text-orange-700 mb-4" />,
-  //     title: "User-Centric & Reliable",
-  //     description:
-  //       "Collaborating closely with UI/UX teams to create intuitive interfaces and reliable systems trusted by leading banks.",
-  //   },
-  // ];
+  //* Benefits
+  const benefits = [
+    {
+      icon: <Scaling className="w-10 h-10 mb-4" />,
+      title: "Scalability",
+    },
+    {
+      icon: <Sparkles className="w-10 h-10 mb-4" />,
+      title: "Efficiency",
+    },
+    {
+      icon: <ShieldCheck className="w-10 h-10 mb-4" />,
+      title: "User-friendly",
+    },
+    {
+      icon: <Scaling className="w-10 h-10 mb-4" />,
+      title: "Robust solutions",
+    },
+    {
+      icon: <Sparkles className="w-10 h-10 mb-4" />,
+      title: "Remarkable website",
+    },
+    {
+      icon: <ShieldCheck className="w-10 h-10 mb-4" />,
+      title: "Mantainable code",
+    },
+  ];
 
   return (
-    <section id="benefits" className="h-fit min-h-svh">
+    <section id="benefits" className="h-fit min-h-svh pt-20">
       <div className="container mx-auto px-6">
         <div className="text-center mb-16">
           <div className="h2-container">
@@ -37,23 +46,17 @@ export default function BenefitsSection() {
               </AnimatedChar>
             ))}
           </div>
-          <p className="text-lg text-gray-600 mt-2">
-            I deliver tangible results, not just code.
-          </p>
-          <div className="w-20 h-1 bg-orange-700 mx-auto mt-4 rounded"></div>
         </div>
-        {/* <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 py-3">
           {benefits.map((benefit, index) => (
-            <div
-              key={index}
-              className="bg-gray-50 p-8 rounded-lg shadow-md text-center hover:shadow-xl hover:-translate-y-2 transition-transform duration-300"
-            >
+            <div key={index} className="benefit-box">
               {benefit.icon}
-              <h3 className="text-xl font-bold mb-2">{benefit.title}</h3>
-              <p className="text-gray-600">{benefit.description}</p>
+              <h3 className="text-xl font-bold mb-2 grow">{benefit.title}</h3>
+              {/* <p className="text-gray-600">{benefit.description}</p> */}
             </div>
           ))}
-        </div> */}
+        </div>
       </div>
     </section>
   );
